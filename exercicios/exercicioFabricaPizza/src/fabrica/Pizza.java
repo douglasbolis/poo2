b16a2e09198f4871981c9bde486eb6415e6e10de
douglasbolis <douglas.lima@xdevel.com.br>
2017-03-08 10:57:54 -0300

    Alterado ordem do abstract.

--- exercicios/exercicioFabricaPizza/src/fabrica/Pizza.java
+++ exercicios/exercicioFabricaPizza/src/fabrica/Pizza.java
@@ -5,8 +5,8 @@ package fabrica;
  * @author douglas
  */
 public abstract class Pizza {
-    abstract public void bake();
-    abstract public void prepare();
-    abstract public void cut();
-    abstract public void box();
+    public abstract void bake();
+    public abstract void prepare();
+    public abstract void cut();
+    public abstract void box();
 }
