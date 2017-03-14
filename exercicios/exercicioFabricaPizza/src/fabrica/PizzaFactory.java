f1fbab68e3648e2b0fde39da3a017ac2a9abe047
douglasbolis <douglas.lima@xdevel.com.br>
2017-03-13 23:13:41 -0300

    Exercício fabrica pizza.

--- exercicios/exercicioFabricaPizza/src/fabrica/PizzaFactory.java
+++ exercicios/exercicioFabricaPizza/src/fabrica/PizzaFactory.java
@@ -5,7 +5,17 @@ package fabrica;
  * @author douglas
  */
 public class PizzaFactory {
-    public static void main(String[] args) {
+    public static Pizza createPizza(String classe) {
         Pizza pizza;
+        Object classeReflection = null;
+        
+	try {
+            classeReflection = Class.forName(classe).newInstance();            
+        }
+        catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+	    e.printStackTrace();
+	}        
+        pizza = (Pizza) classeReflection;
+        return pizza;
     }
 }
--- exercicios/exercicioFabricaPizza/src/fabrica/PizzaStore.java
+++ exercicios/exercicioFabricaPizza/src/fabrica/PizzaStore.java
@@ -5,7 +5,18 @@ package fabrica;
  * @author douglas
  */
 public class PizzaStore {
-    public Pizza orderPizza() {
-        return null;
+    public Pizza orderPizza(String pizza) {
+        String nomeClasse = "fabrica.";
+        if (pizza.equalsIgnoreCase("queijo")) {
+            nomeClasse += "CheesePizza";
+        } else  if (pizza.equalsIgnoreCase("frutos do mar")) {
+            nomeClasse += "ClamPizza";
+        } else if (pizza.equalsIgnoreCase("vegetariano")) {
+            nomeClasse += "VeggiePizza";
+        } else {
+            nomeClasse += "PepperoniPizza";
+        }
+        
+        return PizzaFactory.createPizza(nomeClasse);
     }
 }
