3ba81ffe0b884fa5aefd4b8d709accc38949b3c0
douglasbolis <douglas.lima@xdevel.com.br>
2017-05-02 07:50:58 -0300

    Adicionado exercicio dao.

---
+++ exercicios/exercicioDAOHibernate/src/main/java/example/hibernate/persistencia/VeiculoDAO.java
@@ -0,0 +1,15 @@
+
+
+package example.hibernate.persistencia;
+
+import example.hibernate.model.Veiculo;
+
+/**
+ *
+ * @author felipe
+ */
+public interface VeiculoDAO extends GenericDAO<Veiculo> {
+    
+     
+    
+}
---
+++ exercicios/exercicioDAOHibernate/src/main/java/example/hibernate/persistencia/VeiculoDAOImpl.java
@@ -0,0 +1,17 @@
+
+
+package example.hibernate.persistencia;
+
+import example.hibernate.model.Veiculo;
+
+
+
+/**
+ *
+ * @author felipe
+ */
+public class VeiculoDAOImpl  extends GenericDAOImpl<Veiculo> implements VeiculoDAO {
+
+        
+    
+}
