9f238f6276f03e892fe030f659bb34dd5d0c92a1
douglasbolis <douglas.lima@xdevel.com.br>
2017-05-09 08:15:21 -0300

    Modificado fronteira do exercício.

--- exercicios/exercicioCadeiaResponsabiblidadeCaixaAutomatico/src/cadeia/responsabilidade/fronteira/Entrada.java
+++ exercicios/exercicioCadeiaResponsabiblidadeCaixaAutomatico/src/cadeia/responsabilidade/fronteira/Entrada.java
@@ -2,25 +2,20 @@ package cadeia.responsabilidade.fronteira;
 
 import java.util.Scanner;
 
-public class Entrada {    
-    public static int leInt(String msg) {
-        Scanner reader = new Scanner(System.in);
-
-        Saida.print(msg);
+public class Entrada {
+    public static Scanner reader = new Scanner( System.in );
+    public static int leInt( String msg ) {
+        Saida.println( msg );
         return reader.nextInt();
     }
 
-    public static Double leDouble(String msg) {
-        Scanner reader = new Scanner(System.in);
-
-        Saida.print(msg);
+    public static Double leDouble( String msg ) {
+        Saida.println( msg );
         return reader.nextDouble();
     }
 
-    public static String leString(String msg) {
-        Scanner reader = new Scanner(System.in);
-
-        Saida.print(msg);
+    public static String leString( String msg ) {
+        Saida.println( msg );
         return reader.next();
     }
 }
