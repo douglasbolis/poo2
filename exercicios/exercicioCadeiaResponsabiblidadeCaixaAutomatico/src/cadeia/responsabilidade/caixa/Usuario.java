4c3af6463918eb48674503e5dcf4d104dee44cf5
douglasbolis <douglas.lima@xdevel.com.br>
2017-05-09 07:46:58 -0300

    Exercício cadeia de responsabilidade caixa automático iniciado.

--- exercicios/exercicioCadeiaResponsabiblidadeCaixaAutomatico/src/cadeia/responsabilidade/caixa/Usuario.java
+++ exercicios/exercicioCadeiaResponsabiblidadeCaixaAutomatico/src/cadeia/responsabilidade/caixa/Usuario.java
@@ -7,16 +7,40 @@ import java.util.ArrayList;
  * @author douglas
  */
 public class Usuario implements InterfaceAtm {
-    String nome;
-    Integer Dinheiro;
-    Atm atm;
+    private String name;
+    private Integer money;
+    private Atm atm;
     
-    Usuario( String nome ) {
-        this.nome = nome;
-        atm = new Atm();
+    public Usuario( String name ) {
+        this.setName( name );
+        this.setAtm( new Atm() );
     }
     
     public ArrayList< Integer > executarSaque( Integer numero ) {
         return atm.executarSaque( numero );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney( Integer money ) {
+        this.money = money;
+    }
+
+    public Atm getAtm() {
+        return atm;
+    }
+
+    public void setAtm( Atm atm ) {
+        this.atm = atm;
     }
 }
