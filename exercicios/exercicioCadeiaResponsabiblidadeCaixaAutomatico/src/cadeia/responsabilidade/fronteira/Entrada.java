4d07f2470b4001217f84b9d2403a24d7a3bd117b
douglasbolis <douglas.lima@xdevel.com.br>
2017-05-10 12:44:53 -0300

    Corrigido quebra de linha na entrada de dados.

--- exercicios/exercicioCadeiaResponsabiblidadeCaixaAutomatico/src/cadeia/responsabilidade/fronteira/Entrada.java
+++ exercicios/exercicioCadeiaResponsabiblidadeCaixaAutomatico/src/cadeia/responsabilidade/fronteira/Entrada.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Entrada {
     public static Scanner reader = new Scanner( System.in );
     public static int leInt( String msg ) {
-        Saida.println( msg );
+        Saida.print( msg );
         return reader.nextInt();
     }
 
     public static Double leDouble( String msg ) {
-        Saida.println( msg );
+        Saida.print( msg );
         return reader.nextDouble();
     }
 
     public static String leString( String msg ) {
-        Saida.println( msg );
+        Saida.print( msg );
         return reader.next();
     }
 }
